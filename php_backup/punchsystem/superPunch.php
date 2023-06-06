9d7f110bcc0fd702f4e966f7adade674c24d01d9
DavidNine <acut74569@gmail.com>
2023-06-06 12:41:53 +0800

    backup -- 20230606

--- php_backup/punchsystem/superPunch.php
+++ php_backup/punchsystem/superPunch.php
@@ -16,6 +16,19 @@
     if (!$con) {
         die("Connection failed!" . mysqli_connect_error());
     }
+
+    $chk_acc = "SELECT * FROM `UserData` WHERE `account` = '$account';";
+    $chk_acc_ret = mysqli_query($con,$chk_acc);
+    if ($chk_acc_ret){
+        if (!mysqli_num_rows($chk_acc_ret)) {
+            
+            echo "<script>";
+            echo "alert('Fail to assign salary, Account: $account does not exist.');";
+            echo "window.history.back();";
+            echo "</script>";
+        }
+    }
+
     $sql = "INSERT INTO `punch-record`(`account`, `date`, `time`, `status`) VALUES ('$account','$Date','$Time','$status');";
     $ret = mysqli_query($con,$sql);
 
